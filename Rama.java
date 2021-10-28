c6daf9ac1e8be28284aeed448316e7c91026bfaa
Firdaus Kuncoro <77367898+FirdausKun@users.noreply.github.com>
2021-10-28 22:47:40 +0800

    Rama.java
    jangan lupa label
    hacktoberfest-accepted

---
+++ Rama.java
@@ -0,0 +1,8 @@
+public class Latihan1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.print("I'm New");
+        System.out.println(" Here!");
+    }
+    
+}
